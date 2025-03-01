ed2e6ee9ed5076c2c1c8b695e79005f00eda3be7
Siddhukoraboyina <siddhukoraboina123@gmail.com>
2025-03-01 11:18:35 +0530

    Update array.dart

--- array.dart
+++ array.dart
@@ -1,24 +1,31 @@
-def isSubsetSum(arr, n, sum):
-    dp = [[False for _ in range(sum + 1)] for _ in range(n + 1)]
-    
-    for i in range(n + 1):
-        dp[i][0] = True
-    
-    for i in range(1, n + 1):
-        for j in range(1, sum + 1):
-            if arr[i - 1] <= j:
-                dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
-            else:
-                dp[i][j] = dp[i - 1][j]
-    
-    return dp[n][sum]
+bool isSubsetSum(List<int> arr, int n, int sum) {
+  List<List<bool>> dp = List.generate(n + 1, (i) => List.filled(sum + 1, false));
+  
+  for (int i = 0; i <= n; i++) {
+    dp[i][0] = true;
+  }
+  
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= sum; j++) {
+      if (arr[i - 1] <= j) {
+        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+  
+  return dp[n][sum];
+}
 
-arr1 = [3, 34, 4, 12, 5, 2]
-sum1 = 9
-n1 = len(arr1)
-print(isSubsetSum(arr1, n1, sum1))
-
-arr2 = [3, 34, 4, 12, 5, 2]
-sum2 = 30
-n2 = len(arr2)
-print(isSubsetSum(arr2, n2, sum2))
+void main() {
+  List<int> arr1 = [3, 34, 4, 12, 5, 2];
+  int sum1 = 9;
+  int n1 = arr1.length;
+  print(isSubsetSum(arr1, n1, sum1)); // Output: true
+  
+  List<int> arr2 = [3, 34, 4, 12, 5, 2];
+  int sum2 = 30;
+  int n2 = arr2.length;
+  print(isSubsetSum(arr2, n2, sum2)); // Output: false
+}
